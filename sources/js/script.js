51c80a841658dffe5635df5e158d483c0d677fd8
Yogaisan Ramasawmy <yogaisan@gmail.com>
2022-01-02 19:38:23 +0400

    completed level 3 exercise

--- sources/js/script.js
+++ sources/js/script.js
@@ -7,3 +7,33 @@ $(".owl-carousel").owlCarousel({
     autoplayTimeout: 5000,
     autoplaySpeed: 2000,
 });
+
+const form = document.querySelector('form');
+const emailInput = document.querySelector('[type="email"]');
+const submitBtn = document.querySelector('[type="submit"]')
+const connexionRollover = document.getElementById('connexion-rollover');
+const formLine = document.querySelector('.form-line');
+const emailRegex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+(?:[A-Z]{2}|com|net|org|net|gov|mil|biz|info|mobi|name|uk|aero|org|jobs|museum)\b/;
+
+const displayEmail = (event) => {
+  event.preventDefault();
+  connexionRollover.textContent = emailInput.value;
+  connexionRollover.classList.add('logged-in')
+  emailInput.value = '';
+  submitBtn.disabled = true;
+}
+
+const toggleSubmitBtn = () => {
+  if (emailInput.value === '' || !emailRegex.test(emailInput.value)) {
+    submitBtn.disabled = true;
+    formLine.classList.remove('valid-email');
+    formLine.classList.add('invalid-email');
+  } else {
+    submitBtn.disabled = false;
+    formLine.classList.remove('invalid-email');
+    formLine.classList.add('valid-email');
+  }
+}
+
+form.addEventListener('submit', displayEmail);
+emailInput.addEventListener('keyup', toggleSubmitBtn);
